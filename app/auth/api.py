035c378a965fa080035a6eaa2ae7acf1cf3aba5d
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-24 22:24:08 +0500

    create user endpoints for auth

---
+++ app/auth/api.py
@@ -0,0 +1,72 @@
+from datetime import timedelta
+from typing import Annotated
+
+from fastapi import APIRouter, Depends, HTTPException, status
+from fastapi.security import OAuth2PasswordBearer
+from sqlalchemy.ext.asyncio import AsyncSession
+
+from app.auth.schema import Token, UserRead, UserCreate
+from app.auth.utils import CustomOAuth2PasswordRequestForm, authenticate_user, create_access_token, \
+    get_current_active_user, create_user, blacklist_token
+from app.config import ACCESS_TOKEN_EXPIRE_MINUTES
+from app.database import get_async_session
+
+router = APIRouter()
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="token")
+
+
+@router.post("/login")
+async def login(
+        form_data: Annotated[CustomOAuth2PasswordRequestForm, Depends()],
+        db: AsyncSession = Depends(get_async_session),
+) -> Token:
+    user = await authenticate_user(db, form_data.phone, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect phone number or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=int(ACCESS_TOKEN_EXPIRE_MINUTES))
+    access_token = create_access_token(
+        data={"sub": user.phone}, expires_delta=access_token_expires
+    )
+    return Token(access_token=access_token, token_type="bearer")
+
+
+@router.post("/logout")
+async def logout(
+        current_user: Annotated[UserRead, Depends(get_current_active_user)],
+        token: Annotated[str, Depends(oauth2_scheme)],
+):
+    blacklist_token(token)
+    return {"msg": "Successfully logged out"}
+
+
+@router.post("/create")
+async def register_user(
+        user: UserCreate,
+        current_user: Annotated[UserRead, Depends(get_current_active_user)],
+        db: AsyncSession = Depends(get_async_session),
+):
+    try:
+        if not current_user.is_superuser:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED, detail="You are not authorized to create users")
+        return await create_user(db, user)
+    except Exception as e:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+@router.get("/me/", response_model=UserRead)
+async def read_users_me(
+        current_user: Annotated[UserRead, Depends(get_current_active_user)],
+):
+    return current_user
+
+
+@router.get("/me/items/")
+async def read_own_items(
+        current_user: Annotated[UserRead, Depends(get_current_active_user)],
+):
+    return [{"item_id": "Foo", "owner": current_user.username}]
