e4c9af82f3b3d00a4b2dc4dc604743a105010bab
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-11-29 09:19:14 +0500

    include user endpoints

--- app/auth/__init__.py
+++ app/auth/__init__.py
@@ -1,5 +1,6 @@
 from fastapi import APIRouter
-from .api import router as auth_router
+from .api import router as auth_router, router_user as user_router
 
 router = APIRouter()
-router.include_router(auth_router, prefix="/auth", tags=["auth"])
+router.include_router(auth_router, prefix="/auth", tags=["Auth"])
+router.include_router(user_router, prefix="/user", tags=["User"])
