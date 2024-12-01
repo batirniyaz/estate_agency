5007755025a553386e7d67e2544b9b39c0ca15da
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-01 16:30:22 +0500

    include land router

--- app/__init__.py
+++ app/__init__.py
@@ -2,9 +2,12 @@ from fastapi import APIRouter
 from app.metro.api import router as metro_router
 from app.auth import router as auth_router
 from app.district.api import router as district_router
+from app.object.endpoints.land import router as land_router
 
 router = APIRouter()
 
 router.include_router(auth_router)
 router.include_router(metro_router, prefix="/metro", tags=["Metro"])
 router.include_router(district_router, prefix="/district", tags=["District"])
+router.include_router(land_router, prefix="/land", tags=["Land"])
+
