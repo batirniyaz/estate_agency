8c62d7de31db5b210b4ad6972f3ab3d3fe69b8f0
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-02 12:56:15 +0500

    update and fix a lot of bugs with creating file and add validation of values

--- app/object/functions/land.py
+++ app/object/functions/land.py
@@ -1,12 +1,16 @@
 from fastapi import HTTPException, status, UploadFile
+from fastapi.encoders import jsonable_encoder
 from sqlalchemy import event
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 from sqlalchemy.orm import Session
 
-from app.object.models.land import Land
-from app.object.schemas.land import LandCreate, LandUpdate
+from app.object.models.land import Land, LandMedia
+from app.object.schemas.land import LandCreate, LandUpdate, LandResponse
+from app.utils.file_utils import save_upload_file
+
+from app.object.functions.validations.validate_land import validate_land
 
 
 def generate_crm_id(mapper, connection, target):
@@ -21,11 +25,31 @@ event.listen(Land, 'before_insert', generate_crm_id)
 
 async def create_land(db: AsyncSession, land: LandCreate, media: [UploadFile]):
     try:
-        land = Land(**land.model_dump())
-        db.add(land)
-        await db.commit()
-        await db.refresh(land)
-        return land
+        print(land.phone_number[1:])
+        land_validation = await validate_land(db, land)
+
+        if land_validation:
+            land.responsible = 'agent'
+            db_land = Land(**land.model_dump())
+            db.add(db_land)
+            await db.commit()
+            await db.refresh(db_land)
+
+            urls = save_upload_file(media, db_land.id)
+            for ulr in urls:
+                db_land_image = LandMedia(land_id=db_land.id, url=ulr)
+                db.add(db_land_image)
+                await db.commit()
+                await db.refresh(db_land_image)
+
+                db_land.media.append(db_land_image)
+
+            await db.commit()
+            await db.refresh(db_land)
+
+            print(db_land)
+            land_response = LandResponse.model_validate(db_land)
+            return jsonable_encoder(land_response)
     except IntegrityError as e:
         if 'duplicate key value violates unique constraint' in str(e):
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Land already exists")
@@ -36,5 +60,8 @@ async def create_land(db: AsyncSession, land: LandCreate, media: [UploadFile]):
 
 async def get_land(db: AsyncSession, land_id: int):
     result = await db.execute(select(Land).filter_by(id=land_id))
-    lands = result.scalars().first()
-    return lands if lands else None
+    land = result.scalars().first()
+    if not land:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Land not found")
+
+    return land
