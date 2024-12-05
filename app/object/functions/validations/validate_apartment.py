2765ef964eccb4ebc0021f2333da12323b1c25a9
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-05 22:46:38 +0500

    change some naming in validation

--- app/object/functions/validations/validate_apartment.py
+++ app/object/functions/validations/validate_apartment.py
@@ -5,43 +5,42 @@ from app.district.model import District
 from app.metro.model import Metro
 
 
-async def validate_land(db, land):
-    result = await db.execute(select(District).filter_by(name=land.district))
+async def validate_apartment(db, apartment):
+    result = await db.execute(select(District).filter_by(name=apartment.district))
     district = result.scalars().first()
     if not district:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object district not found")
 
-    result = await db.execute(select(Metro).filter_by(name=land.metro_st))
+    result = await db.execute(select(Metro).filter_by(name=apartment.metro_st))
     metro = result.scalars().first()
     if not metro:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Object metro not found")
 
-    if land.price <= 0:
+    if apartment.price <= 0:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object price must be greater than 0")
-    elif not land.price.is_integer():
+    elif not apartment.price.is_integer():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object price must be integer")
 
-    if land.rooms <= 0 or land.rooms > 30:
+    if apartment.rooms <= 0 or apartment.rooms > 30:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object room must be greater than 0")
-    elif not land.rooms.is_integer():
+    elif not apartment.rooms.is_integer():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object room must be integer")
 
-    if not land.floor.is_integer():
+    if not apartment.floor.is_integer():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object floor must be integer")
-    elif land.floor > land.floor_number:
+    elif apartment.floor > apartment.floor_number:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object floor must be less than floor_number")
 
-    if len(land.phone_number) != 13:
+    if len(apartment.phone_number) != 13:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object phone must be 13 characters")
-    elif not land.phone_number[1:].isdigit():
+    elif not apartment.phone_number[1:].isdigit():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object phone must be integer")
-    elif not land.phone_number[0] == "+":
-        print(land.phone_number[1])
+    elif not apartment.phone_number[0] == "+":
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object phone must start with +")
 
-    if 0 >= land.agent_percent > 100:
+    if 0 >= apartment.agent_percent > 100:
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object agent_percent must be greater than 0")
-    elif not land.agent_percent.is_integer():
+    elif not apartment.agent_percent.is_integer():
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Object agent_percent must be integer")
 
     return True
