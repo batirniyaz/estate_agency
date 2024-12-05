2999aad36c27924258593429466d439127875145
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-05 13:06:14 +0500

    rename from land to apartment

--- app/__init__.py
+++ app/__init__.py
@@ -2,12 +2,12 @@ from fastapi import APIRouter
 from app.metro.api import router as metro_router
 from app.auth import router as auth_router
 from app.district.api import router as district_router
-from app.object.endpoints.land import router as land_router
+from app.object.endpoints.apartment import router as apartment_router
 
 router = APIRouter()
 
 router.include_router(auth_router)
 router.include_router(metro_router, prefix="/metro", tags=["Metro"])
 router.include_router(district_router, prefix="/district", tags=["District"])
-router.include_router(land_router, prefix="/land", tags=["Land"])
+router.include_router(apartment_router, prefix="/apartment", tags=["Apartment"])
 
