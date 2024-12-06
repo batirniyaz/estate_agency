5af883baf237f56ca7f1f1f042c69e29cbf26605
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-06 18:27:39 +0500

    correct the logic and validations of land

--- app/object/functions/land.py
+++ app/object/functions/land.py
@@ -2,11 +2,11 @@ import os
 
 from fastapi import HTTPException, status, UploadFile
 from fastapi.encoders import jsonable_encoder
-from sqlalchemy import event
 from sqlalchemy.exc import IntegrityError
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
+from app.object.functions import generate_crm_id
 from app.object.models.land import LandMedia, Land
 from app.object.schemas.land import LandCreate, LandResponse, LandUpdate
 from app.utils.file_utils import save_upload_file
@@ -14,22 +14,12 @@ from app.utils.file_utils import save_upload_file
 from app.object.functions.validations.validate_land import validate_land
 
 
-async def generate_crm_id(mapper, connection, target):
-    async with AsyncSession(bind=connection) as session:
-        res = await session.execute(select(Land).order_by(LandMedia.id.desc()).limit(1))
-        max_id = res.scalar()
-        next_id = (max_id.id + 1) if max_id else 1
-        target.crm_id = f"L{next_id}"
-
-
-event.listen(Land, 'before_insert', generate_crm_id)
-
-
 async def create_land(db: AsyncSession, land: LandCreate, media: [UploadFile], current_user):
     try:
         land_validation = await validate_land(db, land)
 
         if land_validation:
+            land.crm_id = await generate_crm_id(db, Land, 'L')
             land.responsible = current_user.full_name
             land.agent_commission = land.agent_percent * land.price / 100
             db_land = Land(**land.model_dump())
@@ -39,7 +29,7 @@ async def create_land(db: AsyncSession, land: LandCreate, media: [UploadFile], c
 
             urls = save_upload_file(media, db_land.id, 'land')
             for url in urls:
-                db_land_media = LandMedia(id=db_land.id, url=url['url'], media_type=url['media_type'])
+                db_land_media = LandMedia(land_id=db_land.id, url=url['url'], media_type=url['media_type'])
                 db.add(db_land_media)
                 db_land.media.append(db_land_media)
 
@@ -50,6 +40,7 @@ async def create_land(db: AsyncSession, land: LandCreate, media: [UploadFile], c
             return jsonable_encoder(land_response)
     except IntegrityError as e:
         if 'duplicate key value violates unique constraint' in str(e):
+            print(e)
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Land already exists")
         raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail=str(e))
     except Exception as e:
@@ -79,9 +70,11 @@ async def get_land(db: AsyncSession, land_id: int):
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
 
 
-async def update_land(db: AsyncSession, land_id: int, land: LandUpdate):
+async def update_land(db: AsyncSession, land_id: int, land: LandUpdate, agent_name):
     try:
         db_land = await get_land(db, land_id)
+        if agent_name != db_land.responsible:
+            raise HTTPException(status_code=status.HTTP_401_UNAUTHORIZED, detail='This object created by another agent')
 
         land_validation = await validate_land(db, land)
         if land_validation:
