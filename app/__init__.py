e73d9983960101c6c44afacc86ca36a88c260a3e
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-07 00:24:38 +0500

    include commercial endpoints to base app

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from app.auth import router as auth_router
 from app.district.api import router as district_router
 from app.object.endpoints.apartment import router as apartment_router
 from app.object.endpoints.land import router as land_router
+from app.object.endpoints.commercial import router as commercial_router
 
 router = APIRouter()
 
@@ -12,4 +13,5 @@ router.include_router(metro_router, prefix="/metro", tags=["Metro"])
 router.include_router(district_router, prefix="/district", tags=["District"])
 router.include_router(apartment_router, prefix="/apartment", tags=["Apartment"])
 router.include_router(land_router, prefix='/land', tags=["Land"])
+router.include_router(commercial_router, prefix='/commercial', tags=["Commercial"])
 
