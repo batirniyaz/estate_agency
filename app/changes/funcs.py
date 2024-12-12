2d63629ed9e0f780cebd157d33bc1ab1be96b181
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-12 07:53:10 +0500

    everything there works correctly

--- app/changes/funcs.py
+++ app/changes/funcs.py
@@ -110,7 +110,7 @@ def register_event_listener(model: Type):
         db_session = AsyncSession.object_session(target)
         if db_session:
             asyncio.create_task(
-                log_queue.put((db_session, model.__tablename__, OperationType.DELETE, target.created_by_id,
+                log_queue.put((target.__tablename__, OperationType.DELETE, target.responsible,
                                {column.key: getattr(target, column.key) for column in mapper.columns},
                                None))
             )
