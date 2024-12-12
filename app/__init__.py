bc367532b70fea788840fc54be7570b352ff3f87
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-12 18:23:30 +0500

    include changes endpoint to main app

--- app/__init__.py
+++ app/__init__.py
@@ -5,10 +5,12 @@ from app.district.api import router as district_router
 from app.object.endpoints.apartment import router as apartment_router
 from app.object.endpoints.land import router as land_router
 from app.object.endpoints.commercial import router as commercial_router
+from app.changes.endpoints import router as changes_router
 
 router = APIRouter()
 
 router.include_router(auth_router)
+router.include_router(changes_router, prefix="/changes", tags=["Changes"])
 router.include_router(metro_router, prefix="/metro", tags=["Metro"])
 router.include_router(district_router, prefix="/district", tags=["District"])
 router.include_router(apartment_router, prefix="/apartment", tags=["Apartment"])
