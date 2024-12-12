787b7b77e338d27b7f3d815decc492211e72e1a3
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-12 07:49:43 +0500

    add all models manually

--- app/changes/track_models.py
+++ app/changes/track_models.py
@@ -1,10 +1,14 @@
-from sqlalchemy.orm import registry
 
 from app.changes.funcs import register_event_listener
 
+from app.metro.model import Metro
+from app.auth.model import User
+from app.object.models.land import Land
+from app.object.models.apartment import Apartment
+from app.object.models.commercial import Commercial
+from app.district.model import District
+
 
 def register_event_listeners():
-    mapper_registry = registry()
-    for mapper in mapper_registry.mappers:
-        model = mapper.class_
+    for model in [Land, Metro, User, Apartment, Commercial, District]:
         register_event_listener(model)
