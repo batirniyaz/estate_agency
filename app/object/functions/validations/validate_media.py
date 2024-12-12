0c2bec11cbd60d11e7b3a23b5464f953d8ce2bdd
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-12 22:52:10 +0500

    .

--- app/object/functions/validations/validate_media.py
+++ app/object/functions/validations/validate_media.py
@@ -17,3 +17,4 @@ async def validate_media(media):
                                     detail="Image size is too large")
         else:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid media type")
+        await file.seek(0)
