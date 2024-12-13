15ed7359954863a5a24fd06233a999473d84b8a1
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-14 01:28:19 +0500

    create handlers of bot

---
+++ app/bot/handlers.py
@@ -0,0 +1,20 @@
+from telebot import types
+
+from .instance import bot
+
+from app.config import CHANNEL_ID
+
+id_channel = '-100' + CHANNEL_ID
+
+
+@bot.message_handler(commands=['start', 'help'])
+async def bot_start(message: types.Message):
+    await bot.reply_to(message, 'Hi, Estate Agency bot to post objects to channel')
+
+
+@bot.message_handler(commands=['get_id'])
+async def get_id(message: types.Message):
+    cid = message.chat.id
+    mid = message.message_id
+    fid = message.from_user.id
+    await bot.send_message(message.chat.id, '\n'.join([f'cid: {cid}', f'mid: {mid}', f'fid: {fid}']))
