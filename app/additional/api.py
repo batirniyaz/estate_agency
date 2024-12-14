e3b119536fcccde5a5f47cad57f40a089e05d81f
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-15 04:24:54 +0500

    create and include get media and get media by id funcs

--- app/additional/api.py
+++ app/additional/api.py
@@ -3,7 +3,7 @@ from typing import List
 from fastapi import HTTPException, status, Depends, APIRouter, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 
-from app.additional.delete_media import delete_media
+from app.additional.delete_media import delete_media, get_media_by_id, get_media
 from app.auth.schema import UserRead
 from app.auth.utils import get_current_active_user
 from app.database import get_async_session
@@ -30,3 +30,26 @@ async def delete_media_endpoint(
                            examples=["land", "apartment", "commercial"])
 ):
     return await delete_media(db, media, table)
+
+
+@router.get("/get_media/")
+async def get_media_endpoint(
+        # current_user: UserRead = Depends(get_current_active_user),
+        db: AsyncSession = Depends(get_async_session),
+        table: str = Query(..., title="Table name", description="Table name to get media from",
+                           examples=["land", "apartment", "commercial"]),
+        media_id: int = Query(..., title="Media id", description="Media id to get")
+):
+    return await get_media_by_id(db, table, media_id)
+
+
+@router.get("/get_media_list/")
+async def get_media_list_endpoint(
+        # current_user: UserRead = Depends(get_current_active_user),
+        db: AsyncSession = Depends(get_async_session),
+        table: str = Query(..., title="Table name", description="Table name to get media from",
+                           examples=["land", "apartment", "commercial"]),
+        limit: int = Query(10, title="Limit", description="Limit of media to get"),
+        page: int = Query(1, title="Page", description="Page number")
+):
+    return await get_media(db, table, limit, page)
--- app/additional/delete_media.py
+++ app/additional/delete_media.py
@@ -2,6 +2,7 @@ import os
 from typing import List
 
 from fastapi import HTTPException, status
+from sqlalchemy import func
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy.future import select
 
@@ -23,12 +24,11 @@ async def delete_media(db: AsyncSession, media: List[int], table: str):
         else:
             raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid table name")
         for media_id in media:
-            print(purpose_table)
 
             media_res = await db.execute(select(purpose_table).filter_by(id=media_id))
             media_obj = media_res.scalars().first()
             if not media_obj:
-                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Media not found")
+                raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail=f"Media {media_id} not found")
 
             file_path = os.path.join("app/storage", table.lower(), os.path.basename(media_obj.url))
             if os.path.exists(file_path):
@@ -41,3 +41,42 @@ async def delete_media(db: AsyncSession, media: List[int], table: str):
     except Exception as e:
         await db.rollback()
         raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail=str(e))
+
+
+async def get_media_by_id(db: AsyncSession, table: str, media_id: int):
+
+    purpose_table = None
+    if table.lower() == "land":
+        purpose_table = LandMedia
+    elif table.lower() == "apartment":
+        purpose_table = ApartmentMedia
+    elif table.lower() == "commercial":
+        purpose_table = CommercialMedia
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid table name")
+
+    media_res = await db.execute(select(purpose_table).filter_by(id=media_id))
+    media = media_res.scalars().first()
+    if not media:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Media not found")
+
+    return media
+
+
+async def get_media(db: AsyncSession, table: str, limit: int, page: int):
+
+    purpose_table = None
+    if table.lower() == "land":
+        purpose_table = LandMedia
+    elif table.lower() == "apartment":
+        purpose_table = ApartmentMedia
+    elif table.lower() == "commercial":
+        purpose_table = CommercialMedia
+    else:
+        raise HTTPException(status_code=status.HTTP_400_BAD_REQUEST, detail="Invalid table name")
+
+    total_count = await db.scalar(select(func.count(purpose_table.id)))
+    media_res = await db.execute(select(purpose_table).order_by(purpose_table.id.desc()).limit(limit).offset((page - 1) * limit))
+    media = media_res.scalars().all()
+
+    return {"data": media if media else [], "total_count": total_count}
