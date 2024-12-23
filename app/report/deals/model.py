71c5b8c35042cf5ec9565c5a3976ed6671be7e63
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-23 10:24:56 +0500

    Add Deal model for database representation
    Introduce a new `Deal` model in `app/report/deals/model.py` to represent deals in the database. This includes fields for action type, responsible party, dates, and financial details, with timestamps for creation and updates. The model integrates with SQLAlchemy and uses `ActionType` from existing enums.

---
+++ app/report/deals/model.py
@@ -0,0 +1,27 @@
+import datetime
+
+from sqlalchemy.orm import Mapped, mapped_column
+from sqlalchemy import Integer, String, Enum, TIMESTAMP
+from app.object.models import ActionType
+
+from app.database import Base
+
+
+class Deal(Base):
+    __tablename__ = 'deal'
+
+    id: Mapped[int] = mapped_column(Integer, primary_key=True, index=True)
+    action_type: Mapped[ActionType] = mapped_column(Enum(ActionType))
+    responsible: Mapped[str] = mapped_column(String)
+    date: Mapped[str] = mapped_column(String(10))
+    crm_id: Mapped[str] = mapped_column(String)
+    object_price: Mapped[int] = mapped_column(Integer)
+    commission: Mapped[int] = mapped_column(Integer)
+    agent_percent: Mapped[int] = mapped_column(Integer)
+
+    created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc))
+    updated_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
+                                                          default=lambda: datetime.datetime.now(datetime.timezone.utc),
+                                                          onupdate=lambda: datetime.datetime.now(datetime.timezone.utc))
+
