368461370edfaabbb62e0a6a98977dda5c60aefd
batirniyaz <muratbaevbatirniyaz@gmail.com>
2024-12-26 23:29:44 +0500

    Add Query descriptions for date parameters in Accounting API
    Updated the `start_date`, `end_date`, and `date` query parameters in `get_overall_data` to include descriptions for better API documentation. This improves clarity on the expected date formats for users.

--- app/report/Accounting/api.py
+++ app/report/Accounting/api.py
@@ -1,10 +1,10 @@
-from fastapi import APIRouter, Depends, HTTPException, status
+
+from fastapi import APIRouter, Depends, HTTPException, status, Query
 from sqlalchemy.ext.asyncio import AsyncSession
 from typing import Optional, Annotated
 
 from app.object.models import ActionType
 from app.report.Accounting.funcs import get_overall_data
-from app.report.deals.model import Deal
 from app.database import get_async_session
 from app.auth.utils import get_current_user
 from app.auth.schema import UserRead
@@ -17,9 +17,9 @@ async def get_overall_data_api(
     db: Annotated[AsyncSession, Depends(get_async_session)],
     current_user: Annotated[UserRead, Depends(get_current_user)],
     action_type: ActionType,
-    start_date: Optional[str] = None,
-    end_date: Optional[str] = None,
-    date: Optional[str] = None,
+    start_date: Optional[str] = Query(None, description="Дата начала в формате YYYY-MM-DD"),
+    end_date: Optional[str] = Query(None, description="Дата окончания в формате YYYY-MM-DD"),
+    date: Optional[str] = Query(None, description="Дата в формате YYYY-MM-DD или YYYY-MM или YYYY или YYYY-W52"),
     responsible: Optional[str] = None,
 ):
     if not current_user.is_superuser:
