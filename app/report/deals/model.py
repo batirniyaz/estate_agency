b784479c2e3d3746c8bfdeae00120c29d3346320
batirniyaz <muratbaevbatirniyaz@gmail.com>
2025-01-28 16:44:49 +0500

    add new column to deal model

--- app/report/deals/model.py
+++ app/report/deals/model.py
@@ -1,7 +1,7 @@
 import datetime
 
 from sqlalchemy.orm import Mapped, mapped_column
-from sqlalchemy import Integer, String, Enum, TIMESTAMP
+from sqlalchemy import Integer, String, Enum, TIMESTAMP, Float
 from app.object.models import ActionType
 
 from app.database import Base
@@ -16,8 +16,9 @@ class Deal(Base):
     date: Mapped[str] = mapped_column(String(10))
     crm_id: Mapped[str] = mapped_column(String)
     object_price: Mapped[int] = mapped_column(Integer)
-    commission: Mapped[float] = mapped_column(Integer)
+    commission: Mapped[float] = mapped_column(Float)
     agent_percent: Mapped[int] = mapped_column(Integer)
+    agency_commission: Mapped[float] = mapped_column(Float, nullable=True)
 
     created_at: Mapped[datetime.datetime] = mapped_column(TIMESTAMP(timezone=True),
                                                           default=lambda: datetime.datetime.now(datetime.timezone.utc))
